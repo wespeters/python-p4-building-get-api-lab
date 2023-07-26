fc666cefca7fd8b106332e1b558698b67294139c
Wesley Peters <wesleypeters@gmail.com>
2023-07-26 14:14:43 -0600

    update models.py and app.py

--- server/app.py
+++ server/app.py
@@ -20,19 +20,27 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    response = make_response(jsonify([b.to_dict() for b in bakeries]), 200)
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    response = make_response(jsonify(bakery.to_dict()), 200)
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    response = make_response(jsonify([bg.to_dict() for bg in baked_goods]), 200)
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    response = make_response(jsonify(most_expensive.to_dict()), 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -11,10 +11,22 @@ db = SQLAlchemy(metadata=metadata)
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
+    serialize_rules = ('-baked_goods.bakery',)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    baked_goods = db.relationship('BakedGood', backref='bakery', lazy=True)
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
+    serialize_rules = ('-bakery.baked_goods',)
+
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
